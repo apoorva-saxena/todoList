c1db05a32c077923844eabd4e5f2d3d70ba932a8
apsaxena24 <apsaxena24@gmail.com>
2016-08-18 14:50:12 +0100

    User can sign in

--- spec/features/user_visits_homepage_spec.rb
+++ spec/features/user_visits_homepage_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 feature 'user visits homepage' do
   scenario 'successfully' do
     # visit '/' or we can also use
-    visit root_path
+    # visit root_path
+    sign_in
     expect(page).to have_css 'h1', text: 'Todos'
   end
 end
